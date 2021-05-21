220b4c9bd497252d3c2c190909739e0d6bd634a3
fatih40 <baserfatih40@gmail.com>
2021-05-21 13:53:39 +0200

    continues

--- app/src/main/java/com/fatihbaser/navigationkotlin2/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/fatihbaser/navigationkotlin2/viewmodels/HomeViewModel.kt
@@ -1,15 +1,20 @@
 package com.fatihbaser.navigationkotlin2.viewmodels
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.fatihbaser.navigationkotlin2.models.home.MovieResponse
 import com.fatihbaser.navigationkotlin2.models.home.MovieResult
 import com.fatihbaser.navigationkotlin2.service.ApiClient
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val apiclient=ApiClient()
+    private val apiClient = ApiClient()
     private val disposable=CompositeDisposable()
 
 
@@ -20,6 +25,50 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
 
     fun getPopularMovies(){
+        loadingMovies.value = true
+        disposable.add(
+            apiClient.getPopularMovies()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<MovieResponse>(){
+                    override fun onSuccess(t: MovieResponse) {
+                        popularMovie.value = t.results
+                        loadingMovies.value = false
+                        Log.i("HOME FRAGMENT : ", "POPULAR WORKING")
+                    }
 
+                    override fun onError(e: Throwable) {
+                        Log.i("HOME FRAGMENT : ", "UPCOMING ERROR"+e.message)
+                    }
+
+                })
+        )
     }
+
+    fun getUpcomingMovies(){
+        loadingMovies.value = true
+        disposable.add(
+            apiClient.getUpcomingMovies()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<MovieResponse>(){
+                    override fun onSuccess(t: MovieResponse) {
+                        upcomingMovie.value = t.results
+                        loadingMovies.value = false
+                        Log.i("HOME FRAGMENT : ", "UPCOMING WORKING")
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.i("HOME FRAGMENT : ", "UPCOMING ERROR"+e.message)
+                    }
+
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
 }
